4498a5be1402b749a62d30dd22ab5f78f6728622
David Kahurani <k.kahurani@gmail.com>
2021-11-09 19:32:13 +0300

    register model

--- CRUD/admin.py
+++ CRUD/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from CRUD.models import Quote
+
+class QuoteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Quote,QuoteAdmin)
