cd417e597835bfc135720339538cbe6b948e26f9
David Kahurani <k.kahurani@gmail.com>
2021-11-10 08:55:37 +0300

    Use AutoField instead of IntegerField

--- CRUD/models.py
+++ CRUD/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Quote(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     created_at = models.DateField()
     text = models.CharField(max_length=100)
